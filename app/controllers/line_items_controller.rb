526871aeeb0f6012ccaea715ca696fa11921402e
pulkitarora21 <pulkitarora21@gmail.com>
2014-05-20 01:06:34 +0530

    ajax based cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,9 +31,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        
-        format.html { redirect_to @line_item.cart }
-        format.json { render action: 'show',
+        format.html { redirect_to store_url }
+         format.js 
+         format.json { render action: 'show',
           status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
@@ -73,9 +73,10 @@ class LineItemsController < ApplicationController
       @line_item = LineItem.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the scary internet, only allow the white
+    # list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id) 
+      params.require(:line_item).permit(:product_id)
     end
   #...
 end
