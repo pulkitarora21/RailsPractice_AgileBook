012a6b40c56ad5aef020318e6507738a4abaa142
pulkitarora21 <pulkitarora21@gmail.com>
2014-05-20 20:32:04 +0530

    Internationalization p1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
+  skip_before_action :authorize, only: [:new, :create]
   include CurrentCart
-  skip_before_action :authorize, only: [:new, :create] 
   before_action :set_cart, only: [:new, :create]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
@@ -39,12 +39,11 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-		OrderNotifier.received(@order).deliver
+        OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice: 
-          'Thank you for your order.' }
+          I18n.t('.thanks') }
         format.json { render action: 'show', status: :created,
           location: @order }
-		  
       else
         format.html { render action: 'new' }
         format.json { render json: @order.errors,
