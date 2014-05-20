cc7690c41bc6865a73d56f3b73b78fb0937cff8e
pulkitarora21 <pulkitarora21@gmail.com>
2014-05-20 17:22:06 +0530

    integration testing

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ActionMailer::Base
-  default from: 'Sam Ruby <depot@example.com>'
+  default from: 'Pulkit Ruby <blooddonatebyld@gmail.com>'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
